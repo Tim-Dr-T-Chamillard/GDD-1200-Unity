60056fda560a4bb1e5d43d57c135ff4b105d9cba
Tim-Dr-T-Chamillard <Tim-Dr-T-Chamillard@users.noreply.github.com>
2016-12-26 07:34:20 -0700

    Implemented Start and added prefab to scene

--- UnityTest/Assets/scripts/TeddyBear.cs
+++ UnityTest/Assets/scripts/TeddyBear.cs
@@ -6,7 +6,7 @@ public class TeddyBear : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        print("Hello world from old ASUS");
 	}
 	
 	// Update is called once per frame
